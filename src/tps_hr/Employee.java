d18d920e371db603b2a50a87239102d85f612383
pichayak <pichaya.kar@outlook.com>
2018-12-03 13:27:57 +0700

    class employee modified

--- src/tps_hr/Employee.java
+++ src/tps_hr/Employee.java
@@ -2,15 +2,15 @@ package tps_hr;
 
 import java.util.*;
 
-public abstract class Employee {
-	protected String user;
-	protected String password;
-	protected String name,surname,nickname,tel,address,id,status;
-	protected int age;
-	protected String startingdate,birthdate,resignationdate; //"date/month/year" and "-" if haven't resigned yet.
+public abstract class Employee implements HolidayEntitlement {
+	protected String id, password;
+	protected String name,surname,nickname,tel,address,status;
+	protected int age, salary;
+	protected Date birthDate, startingDate, resignationDate;
 	protected Certificate[] CertificateList;
-	protected PayrollClass[] PayrollList;
-	protected int holidayEntitlement;
+	protected Payroll[] PayrollList;
+	protected TimeSheet[] timeSheetList;
+	protected int workYears, maxHolidays, usedHolidays; // calculating holiday entitlement
 	
 	
 	public Employee() {
@@ -26,95 +26,204 @@ public abstract class Employee {
 		setStartingdate("");
 		setResignationdate("-");
 	}
+
+		public int workDays(Date date, Date startingDate) {
+			
+		}
+		
+		void promote(int newSalary) {
+			this.setSalary(newSalary);
+		}
 	
-	//getter and setter
-	public String getUser() {
-		return user;
-	}
-	public void setUser(String user) {
-		this.user = user;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getSurname() {
-		return surname;
-	}
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
-	public String getNickname() {
-		return nickname;
-	}
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
-	public String getBirthdate() {
-		return birthdate;
-	}
-	public void setBirthdate(String birthdate) {
-		this.birthdate = birthdate;
-	}
-	public String getTel() {
-		return tel;
-	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
+	//getters and setters
 	public String getId() {
 		return id;
 	}
+
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+	public String getNickname() {
+		return nickname;
+	}
+
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+
+	public String getTel() {
+		return tel;
+	}
+
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
 	public String getStatus() {
 		return status;
 	}
+
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
+
 	public int getAge() {
 		return age;
 	}
+
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
+
+	public int getSalary() {
+		return salary;
+	}
+
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+
+	public Date getStartingDate() {
+		return startingDate;
+	}
+
+
+	public void setStartingDate(Date startingDate) {
+		this.startingDate = startingDate;
+	}
+
+
+	public Date getResignationDate() {
+		return resignationDate;
+	}
+
+
+	public void setResignationDate(Date resignationDate) {
+		this.resignationDate = resignationDate;
+	}
+
 
 	public String getStartingdate() {
 		return startingdate;
 	}
 
+
 	public void setStartingdate(String startingdate) {
 		this.startingdate = startingdate;
 	}
+
+
+	public String getBirthdate() {
+		return birthdate;
+	}
+
+
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
 
 	public String getResignationdate() {
 		return resignationdate;
 	}
 
+
 	public void setResignationdate(String resignationdate) {
 		this.resignationdate = resignationdate;
 	}
 
-	public int getHolidayEntitlement() {
-		return holidayEntitlement;
+
+	public Certificate[] getCertificateList() {
+		return CertificateList;
 	}
 
-	public abstract void setHolidayEntitlement() ;
+
+	public void setCertificateList(Certificate[] certificateList) {
+		CertificateList = certificateList;
+	}
+
+
+	public Payroll[] getPayrollList() {
+		return PayrollList;
+	}
+
+
+	public void setPayrollList(Payroll[] payrollList) {
+		PayrollList = payrollList;
+	}
+
+
+	public TimeSheet[] getTimeSheetList() {
+		return timeSheetList;
+	}
+
+
+	public void setTimeSheetList(TimeSheet[] timeSheetList) {
+		this.timeSheetList = timeSheetList;
+	}
+
 	
 }
--- src/tps_hr/HolidayEntitlement.java
+++ src/tps_hr/HolidayEntitlement.java
@@ -1,7 +1,17 @@
 package tps_hr;
 
+import java.util.Date;
+
 public interface HolidayEntitlement {
-	public int getHolidayEntitlement();
-	public void setHolidayEntitlement();
-	public int calculateHolidayEntitlement();
+	
+	
+	public int calculateHolidayEntitlement(int maxHolidays, int usedHolidays);
+	public int decreseHolidays(Date date1, Date date2); //absent for more than 1 day
+	public void reset();
+	public void getWorkYears();
+	public int setWorkYears();
+	public void getmaxHolidays();
+	public int setMaxHolidays();
+	public void getUsedHolidays();
+	public int setUsedHolidays();
 }
