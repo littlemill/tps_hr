d4fac7e46324760ce31c6bc6d63803946b920291
pichayak <pichaya.kar@outlook.com>
2018-12-04 20:11:57 +0700

    main edited

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,49 +1,35 @@
 package GUI;
 
-import tps_hr.TPS;
-
-import javafx.fxml.*;
-
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.scene.Parent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import javafx.scene.control.*;
-import javafx.event.*;
 
-
-
-public class Main extends Application{
+public class Main extends Application {
 	
-	@FXML
-	private Button LoginBtn;
-	private TextField id;
-	private PasswordField password;
-	
+	private Stage primaryStage;
+	private AnchorPane mainLayout;
 	
 	@Override
-	public void start(Stage stage) {
-		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-			
-			Scene scene = new Scene(root);
-			
-			LoginBtn.setOnAction((ActionEvent event)->{
-				
-			});
-			
-			stage.setTitle("The Practical Solution Co.Ltd");
-			stage.setScene(scene);
-			stage.show();
-		}catch(Exception e) {
-			e.printStackTrace();
-		} 
+	public void start(Stage primaryStage) throws IOException {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("The Practical Solution Co.,Ltd");
+		showMainView();
+	}
+	
+	public void showMainView() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("GUI/Login.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene(mainLayout);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
-		
 		launch(args);
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -8,4 +8,5 @@
 module tps_hr {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 }
